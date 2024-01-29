1cc3fd3d7cba2203c6c425edfdb9e3c364b78e79
Thameur Abdellatif <thameur.abdellatif@avanade.com>
2024-01-29 13:29:05 +0100

    Add project

---
+++ src/TodoList.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+interface item {
+    id: number;
+    text: string;
+    completed: boolean;
+
+}
+
+export const TodoList: React.FC = () => {
+    const [todos, setTods] = useState<item[]>([
+        { id: 1, text: "Buy groceries", completed: false },
+        { id: 2, text: "Take a nape", completed: false },
+
+    ])
+    const [input, setInput] = useState<string>("")
+    const handleToggle = (id: number) => {
+
+        setTods(todos.map((todo) => {
+            if (todo.id === id) {
+                return { ...todo, completed: !todo.completed }
+            }
+            return todo;
+        })
+        );
+    }
+    const handleClick = () => {
+        const newTodo: item = { id: Date.now(), text: input, completed: false };
+        setTods([...todos, newTodo]);
+    }
+    const handleRemove = () => {
+        setTods([]);
+    }
+    return <div className='main-container'>
+
+        <h1>To Do List</h1>
+        <ul>
+            {
+                todos.map(todo => (
+                    <li key={todo.id} onClick={() => handleToggle(todo.id)} style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.text} </li>
+                ))}
+        </ul>
+        <input type='text' placeholder='Add todo item' onChange={(e) => setInput(e.currentTarget.value)} />
+        <button onClick={handleClick}>Add</button>
+        <button onClick={handleRemove}>Clear</button>
+
+    </div>;
+
+
+}
+
